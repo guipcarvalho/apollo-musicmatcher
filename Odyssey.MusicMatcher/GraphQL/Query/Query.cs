c682bb5cbf76946b7de39f01b76b6e7453784d56
guipcarvalho <lguilherme.pcarvalho@gmail.com>
2024-11-24 21:40:59 -0300

    Hello world

---
+++ Odyssey.MusicMatcher/GraphQL/Query/Query.cs
@@ -0,0 +1,9 @@
+namespace Odyssey.MusicMatcher.GraphQL.Query;
+
+public class Query
+{
+    public string Hello()
+    {
+        return "Hello world";
+    }
+}
---
+++ Odyssey.MusicMatcher/Program.cs
@@ -0,0 +1,12 @@
+using Odyssey.MusicMatcher.GraphQL.Query;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGraphQLServer()
+    .AddQueryType<Query>();
+
+var app = builder.Build();
+
+app.MapGraphQL();
+
+app.Run();
